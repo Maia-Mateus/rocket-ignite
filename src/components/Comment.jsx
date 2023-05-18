51159d34f02b39e839b1b29e9145a842aff4f39e
Maia-Mateus <matteusmaia889@gmail.com>
2023-05-18 15:41:49 -0300

    I added functionality to the applaud button

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,6 +1,7 @@
 import { ThumbsUp, Trash } from '@phosphor-icons/react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
+import { useState } from 'react'
 
 export function Comment ({ content, onDeleteComment }){
 
@@ -8,6 +9,13 @@ export function Comment ({ content, onDeleteComment }){
         onDeleteComment(content)
     }
 
+    const [likeCount, setLikeCount] = useState(0)
+
+    function handleLikeComment(){
+        setLikeCount((state) => {
+            return state + 1
+        })
+    }
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/131715627?v=4" />
@@ -29,9 +37,9 @@ export function Comment ({ content, onDeleteComment }){
                 </div>
             
             <footer>
-                <button>
+                <button onClick={handleLikeComment}>
                     <ThumbsUp />
-                    Aplaudir <span>20</span>
+                    Aplaudir <span >{likeCount}</span>
                 </button>
             </footer>
             </div>
